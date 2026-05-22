c0aee4de4bfbc9014646c6579d58a7daf6ca6dc4
Munthasir Rahman <munthasirrahman@gmail.com>
2026-05-23 04:21:50 +0600

    feat: add products, stocks, and stock_transactions migrations and models

---
+++ database/migrations/2026_05_23_041438_create_stocks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->enum('size', ['S', 'M', 'L', 'XL', 'XXL']);
+            $table->integer('quantity')->default(0)->unsigned();
+            $table->timestamps();
+
+            $table->unique(['product_id', 'size']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
